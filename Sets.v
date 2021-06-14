be38b6c28b9b274a98415c899552ee27163a9d17
Mateusz Urbańczyk <mu@qed.ai>
2021-06-14 20:05:30 +0200

    Subset diff lemma

--- Sets.v
+++ Sets.v
@@ -8,3 +8,11 @@ Module StrSets := Coq.MSets.MSetWeakList.Make String_as_OT.
 
 Definition StrSet := StrSets.t.
 
+Lemma subset_diff : forall S x, Sets.StrSets.Subset (Sets.StrSets.diff S x) S.
+Proof.
+  unfold StrSets.Subset. intros.
+  apply StrSets.diff_spec in H.
+  destruct H as [H1 H2].
+  apply H1.
+Qed.
+
